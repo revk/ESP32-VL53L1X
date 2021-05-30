9515ec67731b77ac08c9f60350f8b81567166489
Adrian Kennard <a@k.gg>
2021-05-30 16:43:21 +0100

    Starting

--- include/vl53l1x.h
+++ include/vl53l1x.h
@@ -1,26 +1,23 @@
 // VL53L0X control
-// Copyright Â ©2019 Adrian Kennard, Andrews & Arnold Ltd.See LICENCE file for details
-   .GPL 3.0
+// Copyright ©2021 Adrian Kennard, Andrews & Arnold Ltd, and original autors. See LICENCE file for details .GPL 3.0
 
 #ifndef VL53L0X_H
 #define VL53L0X_H
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
 #include <unistd.h>
-#include <malloc.h>
+#include <time.h>
 
-      typedef struct vl53l1x_s vl53l1x_t;
+typedef struct vl53l1x_s vl53l1x_t;
+typedef	uint8_t bool;
 
-enum DistanceMode
-{
+typedef enum {
    VL53L1X_Short, VL53L1X_Medium, VL53L1X_Long, VL53L1X_Unknown
-};
+}vl53l1x_DistanceMode;
 
-enum
-{
+typedef enum {
    VL53L1X_RangeValid = 0,
    VL53L1X_SigmaFail = 1,
    VL53L1X_SignalFail = 2,
@@ -32,78 +29,55 @@ enum
    VL53L1X_ProcessingFail = 8,
    VL53L1X_XtalkSignalFail = 9,
    VL53L1X_SynchronizationInt = 10,
-   VL53L1X_RangeValid MergedPulse = 11,
+   VL53L1X_RangeValid_MergedPulse = 11,
    VL53L1X_TargetPresentLackOfSignal = 12,
    VL53L1X_MinRangeFail = 13,
    VL53L1X_RangeInvalid = 14,
    VL53L1X_None = 255,
-};
+}vl53l1x_RangeStatus;
 
-                vl53l1x_t * vl53l1x_config(int8_t port, int8_t scl, int8_t sda, int8_t xshut, uint8_t address, uint8_t io_2v8);
-const char     *vl53l1x_init(vl53l1x_t *);
-void            vl53l1x_end(vl53l1x_t *);
+vl53l1x_t *vl53l1x_config(int8_t port, int8_t scl, int8_t sda, int8_t xshut, uint8_t address, uint8_t io_2v8);
+const char *vl53l1x_init(vl53l1x_t *);
+void vl53l1x_end(vl53l1x_t *);
 
-void            vl53l1x_setAddress(vl53l1x_t *, uint8_t new_addr);
-uint8_t         vl53l1x_getAddress(vl53l1x_t * v);
+void vl53l1x_setAddress(vl53l1x_t *, uint8_t new_addr);
+uint8_t vl53l1x_getAddress(vl53l1x_t * v);
 
-void            vl53l1x_writeReg8Bit(vl53l1x_t *, uint8_t reg, uint8_t value);
-void            vl53l1x_writeReg16Bit(vl53l1x_t *, uint8_t reg, uint16_t value);
-void            vl53l1x_writeReg32Bit(vl53l1x_t *, uint8_t reg, uint32_t value);
-uint8_t         vl53l1x_readReg8Bit(vl53l1x_t *, uint8_t reg);
-uint16_t        vl53l1x_readReg16Bit(vl53l1x_t *, uint8_t reg);
-uint32_t        vl53l1x_readReg32Bit(vl53l1x_t *, uint8_t reg);
+void vl53l1x_writeReg8Bit(vl53l1x_t *, uint16_t reg, uint8_t value);
+void vl53l1x_writeReg16Bit(vl53l1x_t *, uint16_t reg, uint16_t value);
+void vl53l1x_writeReg32Bit(vl53l1x_t *, uint16_t reg, uint32_t value);
+uint8_t vl53l1x_readReg8Bit(vl53l1x_t *, uint16_t reg);
+uint16_t vl53l1x_readReg16Bit(vl53l1x_t *, uint16_t reg);
+uint32_t vl53l1x_readReg32Bit(vl53l1x_t *, uint16_t reg);
 
-void            vl53l1x_writeMulti(vl53l1x_t *, uint8_t reg, uint8_t const *src, uint8_t count);
-void            vl53l1x_readMulti(vl53l1x_t *, uint8_t reg, uint8_t * dst, uint8_t count);
+void vl53l1x_writeMulti(vl53l1x_t *, uint16_t reg, uint8_t const *src, uint8_t count);
+void vl53l1x_readMulti(vl53l1x_t *, uint16_t reg, uint8_t * dst, uint8_t count);
 
+//bool vl53l1x_setDistanceMode(vl53l1x_t *,vl53l1x_DistanceMode mode);
+vl53l1x_DistanceMode vl53l1x_getDistanceMode(vl53l1x_t *);
 
-bool            vl53l1x_setDistanceMode(DistanceMode mode);
-DistanceMode
-vl53l1x_getDistanceMode()
-{
-   return distance_mode;
-}
+//bool vl53l1x_setMeasurementTimingBudget(vl53l1x_t *,uint32_t budget_us);
+uint32_t vl53l1x_getMeasurementTimingBudget(vl53l1x_t *);
 
-bool            vl53l1x_setMeasurementTimingBudget(uint32_t budget_us);
-uint32_t        vl53l1x_getMeasurementTimingBudget();
+void vl53l1x_setROISize(vl53l1x_t *,uint8_t width, uint8_t height);
+void vl53l1x_getROISize(vl53l1x_t *,uint8_t * width, uint8_t * height);
+void vl53l1x_setROICenter(vl53l1x_t *,uint8_t spadNum);
+uint8_t vl53l1x_getROICenter(vl53l1x_t *);
 
-void            vl53l1x_setROISize(uint8_t width, uint8_t height);
-void            vl53l1x_getROISize(uint8_t * width, uint8_t * height);
-void            vl53l1x_setROICenter(uint8_t spadNum);
-uint8_t         vl53l1x_getROICenter();
+void vl53l1x_startContinuous(vl53l1x_t *,uint32_t period_ms);
+void vl53l1x_stopContinuous(vl53l1x_t *);
+//uint16_t vl53l1x_read(vl53l1x_t *,bool blocking);
+uint16_t vl53l1x_readRangeContinuousMillimeters(vl53l1x_t *,bool blocking);
 
-void            vl53l1x_startContinuous(uint32_t period_ms);
-void            vl53l1x_stopContinuous();
-uint16_t        vl53l1x_read(bool blocking = true);
-uint16_t 
-vl53l1x_readRangeContinuousMillimeters(bool blocking = true)
-{
-   return read(blocking);
-} //alias of read()
-   uint16_t        vl53l1x_readSingle(bool blocking = true);
-   uint16_t        vl53l1x_readRangeSingleMillimeters(bool blocking = true)
-{
-   return readSingle(blocking);
-} //alias of readSingle()
+//uint16_t vl53l1x_readSingle(vl53l1x_t *,bool blocking );
+uint16_t vl53l1x_readRangeSingleMillimeters(vl53l1x_t *,bool blocking );
 
-// check if sensor
-   has new reading available
-      // assumes interrupt is active low(GPIO_HV_MUX__CTRL bit 4 is 1)
-   bool            vl53l1x_dataReady()
-{
-   return (readReg(GPIO__TIO_HV_STATUS) & 0x01) == 0;
-}
+bool vl53l1x_dataReady(vl53l1x_t *);
 
-static const char *vl53l1x_rangeStatusToString(RangeStatus status);
+const char *vl53l1x_rangeStatusToString(vl53l1x_t *,vl53l1x_RangeStatus status);
 
-void
-vl53l1x_setTimeout(uint16_t timeout)
-{
-   io_timeout = timeout;
-}
-uint16_t
-vl53l1x_getTimeout()
-{
-   return io_timeout;
-}
-bool            vl53l1x_timeoutOccurred();
+void vl53l1x_setTimeout(vl53l1x_t *,uint16_t timeout);
+uint16_t vl53l1x_getTimeout(vl53l1x_t *);
+//bool vl53l1x_timeoutOccurred(vl53l1x_t *);
+
+#endif
